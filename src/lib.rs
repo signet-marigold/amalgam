a9bf47a8a680b2ea176d9ecf1068941755fbc813
dan <contact@danmyers.net>
2025-03-17 20:35:25 -0500

    hello amalgam!

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
-mod utils;
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, Amalgam!");
+}
