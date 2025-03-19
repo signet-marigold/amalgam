9186674a934b1186ed69f475943276b8ace78c53
dan <contact@danmyers.net>
2025-03-18 19:47:01 -0500

    updated app

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use leptos::prelude::*;
 
+#[component]
+fn App() -> impl IntoView {
+  let (count, set_count) = signal(0);
+
+  view ! {
+    <button
+      on:click=move |_| set_count.set(3)
+    >
+      "Click me:"
+      {count}
+    </button>
+    <p>
+      "Double count: "
+      {move || count.get() * 2}
+    </p>
+  }
+}
+
 fn main() {
-    console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(|| view! { <p>"Hello world!"</p> })
+  console_error_panic_hook::set_once();
+  mount_to_body(App);
 }
