c3368cbee1d4aaae39d58dc8e45e23b4f291f395
Alex Hack <perad@sqwu.me>
2025-03-21 00:56:56 -0500

    add error notifications

--- src/components/ErrorNotification.tsx
+++ src/components/ErrorNotification.tsx
@@ -3,7 +3,10 @@ interface ErrorNotificationProps {
 
 const ErrorNotification: React.FC<ErrorNotificationProps> = ({ }) => {
   return (
-
+    <div id="error-notification" className="notification error hidden">
+      <span id="error-message"></span>
+      <button className="close-btn">×</button>
+    </div>
   );
 }
 
