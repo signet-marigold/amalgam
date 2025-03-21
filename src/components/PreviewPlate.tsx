e5cccdd2f246bc1898fa21974016e57029ed0114
Alex Hack <perad@sqwu.me>
2025-03-21 00:54:40 -0500

    add preview

---
+++ src/components/PreviewPlate.tsx
@@ -0,0 +1,18 @@
+interface PreviewPlateProps {
+}
+
+const PreviewPlate: React.FC<PreviewPlateProps> = ({ }) => {
+  return (
+    <section className="preview-container">
+      <div className="preview-wrapper">
+        <canvas id="preview-canvas"></canvas>
+        <div className="preview-controls">
+          <button id="play-pause-btn" className="btn icon">▶</button>
+          <div id="time-display" className="time-display">00:00:00 / 00:00:00</div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default PreviewPlate;
