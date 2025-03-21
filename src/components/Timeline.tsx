f3382b121f37ae47a401dd47cbd18fb5f8576448
Alex Hack <perad@sqwu.me>
2025-03-21 00:56:08 -0500

    add timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -3,7 +3,22 @@ interface TimelineProps {
 
 const Timeline: React.FC<TimelineProps> = ({ }) => {
   return (
-
+    <section className="timeline-container">
+      <div className="timeline-controls">
+        <button id="zoom-in-btn" className="btn small button-small">+</button>
+        <button id="zoom-out-btn" className="btn small">-</button>
+        <button id="split-btn" className="btn small">Split</button>
+      </div>
+      <div id="timeline" className="timeline">
+        <div className="timeline-ruler" id="timeline-ruler"></div>
+        <div className="timeline-tracks" id="timeline-tracks">
+          <div className="track video-track" id="video-track-1">
+          </div>
+          <div className="track audio-track" id="audio-track-1">
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
