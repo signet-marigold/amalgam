1d593ccd4d34933188223031a57f29e1ef7daec6
Alex Hack <perad@sqwu.me>
2025-03-21 00:54:05 -0500

    add toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -3,7 +3,20 @@ interface ToolbarProps {
 
 const Toolbar: React.FC<ToolbarProps> = ({ }) => {
   return (
-
+    <div>
+      <button>Import Media</button>
+      <button>Export Video</button>
+      <select id="resolution-select" defaultValue="1080p">
+        <option value="720p">720p</option>
+        <option value="1080p">1080p</option>
+        <option value="4k">4K</option>
+      </select>
+      <select id="framerate-select" defaultValue="30">
+        <option value="24">24 fps</option>
+        <option value="30">30 fps</option>
+        <option value="60">60 fps</option>
+      </select>
+    </div>
   );
 }
 
